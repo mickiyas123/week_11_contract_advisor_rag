3c08f6eaee849de313e2396d4bb6364cbd77f2e0
mickiyas123 <mikikinfe345@gmail.com>
2024-02-19 14:38:59 +0300

    feat: add live reload to front-end docker

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-2xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-2xl font-bold">
+      Hello world Docker is working with live update
+    </h1>
+  );
 }
 
 export default App;
