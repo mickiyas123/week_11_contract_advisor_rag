419b750a5f58b74354d35b133d30c633d4bd7908
mickiyas123 <mikikinfe345@gmail.com>
2024-03-02 06:42:23 +0300

    feat: change chains to get a better response

--- final/app/server.py
+++ final/app/server.py
@@ -2,9 +2,23 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
 from app.rag_chain import final_chain
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
 
+
+# Set all CORS enabled origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
+
+app.mount("/rag/static", StaticFiles(directory="../data"), name="static")
 
 @app.get("/")
 async def redirect_root_to_docs():
